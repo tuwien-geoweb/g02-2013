3bc751aa1bbd93e8bb8aab921c42ffd00a48d261
tualex <scholz0alex@gmail.com>
2013-11-11 16:51:11 +0100

    Update feedback_send.php

--- feedback_send.php
+++ feedback_send.php
@@ -37,7 +37,7 @@
   // SQL-String zusammensetzen 
   $sql = "INSERT INTO feedback (f_name,f_mail,f_anrede,f_msg,f_geoweb,f_datum, geom1)"; 
   $sql = $sql . " VALUES ('" . $name . "','" . $email . "','" . $anrede .  
-         "','" . $message . "'," . $teamflag . ",'" . date("d-m-Y") . "', '" . $lat . "')"; 
+         "','" . $message . "'," . $teamflag . ",'" . date("d-m-Y") . "', '" . asdf . "')"; 
 
   // SQL-String an Datenbank-Server schicken (Beispiel SQLite-Datenbank:  
   $db->exec($sql) or die ('Fehler bei Speichern: '.$db->lastErrorMsg()); 
