cdfcc472634f54d269ba1b7afea90e6565c00106
bernadette123 <bernadette.gugerell@gmail.com>
2014-01-11 17:29:11 +0100

    Update app.js

--- wohnstandort/app.js
+++ wohnstandort/app.js
@@ -5,7 +5,7 @@ var osmLayer = new ol.layer.Tile({source: new ol.source.OSM()});
 var wmsLayer = new ol.layer.Image({
   source: new ol.source.ImageWMS({
     url: '/geoserver/wms',
-    params: {'LAYERS': 'g02_2013:ZAEHLGEBIETOGD'}
+    params: {'LAYERS': 'g02_2013:ZAEHLBEZIRKOGD'}
   }),
   opacity: 0.6
 });
