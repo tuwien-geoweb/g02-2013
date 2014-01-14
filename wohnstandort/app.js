09978e53bffb3f9f86175998869641e05015ec9d
bernadette123 <bernadette.gugerell@gmail.com>
2014-01-14 19:45:12 +0100

    Update app.js

--- wohnstandort/app.js
+++ wohnstandort/app.js
@@ -94,16 +94,23 @@ form.onsubmit = function(evt) {
 
 var kindergarten1 = new ol.layer.Vector({
   source: new ol.source.Vector({
-    url: '/geoserver/g02_2013/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=g02_2013:KINDERGARTENOGD&outputFormat=json',
+    url: 'http://student.ifip.tuwien.ac.at/http://student.ifip.tuwien.ac.at/geoserver/g02_2013/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=g02_2013:KINDERGARTENOGD&outputFormat=json',
     parser: new ol.parser.GeoJSON()
   }),
-});  
+            style: new ol.style.Style({
+                     symbolizers: [
+               new ol.style.Icon({
+                        url: '/kindergarten.png',
+                 })
+                  ]
+            })
+  }); 
 
   
 document.getElementById('kindergarten').onclick = function(e){
   if(this.checked==1){
-    map.addLayer(kindergarten1);
+    olMap.addLayer(kindergarten1);
   }else{
-    map.removeLayer(kindergarten1);
+    olMap.removeLayer(kindergarten1);
   }
-};  
+};
