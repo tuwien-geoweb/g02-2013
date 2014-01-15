d29cd5bc8ab7f745487a6fb27db617281451c70a
bernadette123 <bernadette.gugerell@gmail.com>
2014-01-15 20:38:48 +0100

    Update app1.js

--- wohnstandort/app1.js
+++ wohnstandort/app1.js
@@ -1,55 +1,17 @@
-var layer = new ol.layer.Tile({
-  source: new ol.source.MapQuest({layer: 'sat'})
-});
+map = new OpenLayers.Map("mapdiv");
+    map.addLayer(new OpenLayers.Layer.OSM());
 
-var map = new ol.Map({
-  layers: [layer],
-  renderers: ol.RendererHints.createFromQueryData(),
-  target: 'map',
-  view: new ol.View2D({
-    center: [0, 0],
-    zoom: 2
-  })
-});
+    var lonLat = new OpenLayers.LonLat( -0.1279688 ,51.5077286 )
+          .transform(
+            new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+            map.getProjectionObject() // to Spherical Mercator Projection
+          );
+          
+    var zoom=16;
 
-var pos = ol.proj.transform([16.3725, 48.208889], 'EPSG:4326', 'EPSG:3857');
-
-// Vienna marker
-var marker = new ol.Overlay({
-  position: pos,
-  positioning: ol.OverlayPositioning.CENTER_CENTER,
-  element: document.getElementById('marker'),
-  stopEvent: false
-});
-map.addOverlay(marker);
-
-// Vienna label
-var vienna = new ol.Overlay({
-  position: pos,
-  element: document.getElementById('vienna')
-});
-map.addOverlay(vienna);
-
-// Popup showing the position the user clicked
-var popup = new ol.Overlay({
-  element: document.getElementById('popup')
-});
-map.addOverlay(popup);
-
-map.on('singleclick', function(evt) {
-  var element = popup.getElement();
-  var coordinate = evt.getCoordinate();
-  var hdms = ol.coordinate.toStringHDMS(ol.proj.transform(
-      coordinate, 'EPSG:3857', 'EPSG:4326'));
-
-  $(element).popover('destroy');
-  popup.setPosition(coordinate);
-  // the keys are quoted to prevent renaming in ADVANCED_OPTIMIZATIONS mode.
-  $(element).popover({
-    'placement': 'top',
-    'animation': false,
-    'html': true,
-    'content': '<p>The location you clicked was:</p><code>' + hdms + '</code>'
-  });
-  $(element).popover('show');
-});
+    var markers = new OpenLayers.Layer.Markers( "Markers" );
+    map.addLayer(markers);
+    
+    markers.addMarker(new OpenLayers.Marker(lonLat));
+    
+    map.setCenter (lonLat, zoom);
