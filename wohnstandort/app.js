869625f8a23c80d480862e9737e5219008aaae5c
bernadette123 <bernadette.gugerell@gmail.com>
2014-01-15 19:58:22 +0100

    Update app.js

--- wohnstandort/app.js
+++ wohnstandort/app.js
@@ -1,6 +1,15 @@
 // Base map
 var osmLayer = new ol.layer.Tile({source: new ol.source.OSM()});
 
+    var markers = new OpenLayers.Layer.Markers( "Markers" );
+    map.addLayer(markers);
+
+    var size = new OpenLayers.Size(21,25);
+    var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+    var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
+    markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(0,0),icon));
+    markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(0,0),icon.clone()));
+
 
 // Census map layer
 var wmsLayer = new ol.layer.Image({
