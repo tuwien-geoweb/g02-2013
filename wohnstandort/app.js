56b66915d5da2dbb3ec012c6e15985d8a9725f24
bernadette123 <bernadette.gugerell@gmail.com>
2014-01-15 20:04:13 +0100

    Update app.js

--- wohnstandort/app.js
+++ wohnstandort/app.js
@@ -152,6 +152,12 @@ olMap = new ol.Map({
 var markers = new OpenLayers.Layer.Markers( "Markers" );
 map.addLayer(markers);
 
+var size = new OpenLayers.Size(21,25);
+var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
+markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(0,0),icon));
+markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(0,0),icon.clone()));
+
 // Add behaviour to dropdown
 var topics = document.getElementById('topics');
 topics.onchange = function() {
