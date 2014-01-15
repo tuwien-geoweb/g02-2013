d0a82c90437d981c608f939ddc625a2ea549d13d
bernadette123 <bernadette.gugerell@gmail.com>
2014-01-15 20:07:30 +0100

    Update app.js

--- wohnstandort/app.js
+++ wohnstandort/app.js
@@ -291,3 +291,12 @@ document.getElementById('ubahn').onclick = function(e){
     olMap.removeLayer(ubahn);
   }
 };
+
+var markers = new OpenLayers.Layer.Markers( "Markers" );
+map.addLayer(markers);
+
+var size = new OpenLayers.Size(21,25);
+var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
+markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(16.37,48.21),icon));
+markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(16.37,48.21),icon.clone()));
