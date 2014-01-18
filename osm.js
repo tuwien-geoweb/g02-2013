04ee3f000ffddda4d14295ad581deeb8c86f6e49
tualex <scholz0alex@gmail.com>
2014-01-18 12:20:54 +0100

    Update osm.js

--- osm.js
+++ osm.js
@@ -15,25 +15,3 @@ var map = new ol.Map({
 var geolocation = new ol.Geolocation();
 geolocation.bindTo('projection', map.getView());
 
-var marker = new ol.Overlay({
-  map: map,
-  element: /** @type {Element} */ ($('<i/>').addClass('icon-flag').get(0))
-});
-// bind the marker position to the device location.
-marker.bindTo('position', geolocation);
-
-geolocation.on('change:accuracy', function() {
-  $(marker.getElement()).tooltip({
-    title: this.getAccuracy() + 'm from this point'
-  });
-});
-geolocation.on('error', function(error) {
-  var info = document.getElementById('info');
-  info.innerHTML = error.message;
-  info.style.display = '';
-});
-
-
-$('#locate').click(function() {
-  geolocation.setTracking(true);
-});
