d41a871cfae5fe4ba79e82d016492ca4b1dfc9f8
tualex <scholz0alex@gmail.com>
2014-01-18 12:31:05 +0100

    Update osm.js

--- osm.js
+++ osm.js
@@ -1,13 +1,15 @@
-var osmLayer = new ol.layer.Tile({source: new ol.source.OSM()});
 
-olMap = new ol.Map({
-  target: 'map',
+var map = new ol.Map({
+  layers: [
+    new ol.layer.TileLayer({
+      source: new ol.source.OSM()
+    })
+  ],
   renderer: ol.RendererHint.CANVAS,
-  layers: [osmLayer],
+  target: 'map',
   view: new ol.View2D({
     center: ol.proj.transform([16.37, 48.21], 'EPSG:4326', 'EPSG:3857'),
-    zoom: 11,
-    maxZoom: 18
+    zoom: 11
   })
 });
 
