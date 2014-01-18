d8ebb979b0699aba8885fe6b932b41ca82f939f7
tualex <scholz0alex@gmail.com>
2014-01-18 12:08:11 +0100

    Update osm.js

--- osm.js
+++ osm.js
@@ -44,7 +44,45 @@ $('#locate').click(function() {
 var form = document.forms[0];
 form.onsubmit = function(evt) {
   var url = 'http://nominatim.openstreetmap.org/search?format=json&q=';
-  url += form.query.value;
+  url += form.query.value;var map = new ol.Map({
+  layers: [
+    new ol.layer.TileLayer({
+      source: new ol.source.OSM()
+    })
+  ],
+  renderers: ol.RendererHints.createFromQueryData(),
+  target: 'map',
+  view: new ol.View2D({
+    center: [0, 0],
+    zoom: 2
+  })
+});
+
+var geolocation = new ol.Geolocation();
+geolocation.bindTo('projection', map.getView());
+
+var marker = new ol.Overlay({
+  map: map,
+  element: /** @type {Element} */ ($('<i/>').addClass('icon-flag').get(0))
+});
+// bind the marker position to the device location.
+marker.bindTo('position', geolocation);
+
+geolocation.on('change:accuracy', function() {
+  $(marker.getElement()).tooltip({
+    title: this.getAccuracy() + 'm from this point'
+  });
+});
+geolocation.on('error', function(error) {
+  var info = document.getElementById('info');
+  info.innerHTML = error.message;
+  info.style.display = '';
+});
+
+
+$('#locate').click(function() {
+  geolocation.setTracking(true);
+});
   var xhr = new XMLHttpRequest();
   xhr.open("GET", url, true);
   xhr.onload = function() {
