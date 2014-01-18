dcbad9cac08ca8993cb09d2b2d369bccd3389e5a
tualex <scholz0alex@gmail.com>
2014-01-18 12:35:04 +0100

    Update osm.js

--- osm.js
+++ osm.js
@@ -1,20 +1,21 @@
+var osm = new ol.layer.Tile({
+      source: new ol.source.OSM()
+    });
+
+var view = new ol.View2D({center: ol.proj.transform([16.37, 48.21], 'EPSG:4326', 'EPSG:3857'),
+    zoom: 12,
+    maxZoom: 18
+    });
 
 var map = new ol.Map({
-  layers: [
-    new ol.layer.TileLayer({
-      source: new ol.source.OSM()
-    })
-  ],
-  renderer: ol.RendererHint.CANVAS,
+  layers: [osm],
+  renderers: ol.RendererHints.CANVAS,
   target: 'map',
-  view: new ol.View2D({
-    center: ol.proj.transform([16.37, 48.21], 'EPSG:4326', 'EPSG:3857'),
-    zoom: 11
-  })
+  view: view
 });
 
 var geolocation = new ol.Geolocation();
-geolocation.bindTo('projection', map.getView());
+geolocation.bindTo('projection', map.view());
 
 var marker = new ol.Overlay({
   map: map,
