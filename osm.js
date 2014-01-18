3dac25be65ab8720f9045914a53c3239fd00301e
tualex <scholz0alex@gmail.com>
2014-01-18 11:28:36 +0100

    Update osm.js

--- osm.js
+++ osm.js
@@ -19,9 +19,29 @@ olMap = new ol.Map({
 
 //geolocation
 var geolocation = new ol.Geolocation();
-geolocation.setTracking(true); // here the browser may ask for confirmation
-geolocation.on('change:position', function() {
-  console.log(geolocation.getPosition());
+geolocation.bindTo('projection', map.getView());
+
+var marker = new ol.Overlay({
+  map: map,
+  element: /** @type {Element} */ ($('<i/>').addClass('icon-flag').get(0))
+});
+// bind the marker position to the device location.
+marker.bindTo('position', geolocation);
+
+geolocation.on('change:accuracy', function() {
+  $(marker.getElement()).tooltip({
+    title: this.getAccuracy() + 'm from this point'
+  });
+});
+geolocation.on('error', function(error) {
+  var info = document.getElementById('info');
+  info.innerHTML = error.message;
+  info.style.display = '';
+});
+
+
+$('#locate').click(function() {
+  geolocation.setTracking(true);
 });
 
 // Submit query to Nominatim and zoom map to the result's extent
