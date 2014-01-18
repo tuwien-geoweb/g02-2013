078ed6a4dfd95efc5e2eb4ead429571e822512a3
tualex <scholz0alex@gmail.com>
2014-01-18 12:16:39 +0100

    Update osm.js

--- osm.js
+++ osm.js
@@ -12,52 +12,6 @@ var map = new ol.Map({
   })
 });
 
-
-
-//geolocation
-var geolocation = new ol.Geolocation();
-geolocation.bindTo('projection', map.getView());
-
-var marker = new ol.Overlay({
-  map: map,
-});
-// bind the marker position to the device location.
-marker.bindTo('position', geolocation);
-
-geolocation.on('change:accuracy', function() {
-  $(marker.getElement()).tooltip({
-    title: this.getAccuracy() + 'm from this point'
-  });
-});
-geolocation.on('error', function(error) {
-  var info = document.getElementById('info');
-  info.innerHTML = error.message;
-  info.style.display = '';
-});
-
-
-$('#locate').click(function() {
-  geolocation.setTracking(true);
-});
-
-// Submit query to Nominatim and zoom map to the result's extent
-var form = document.forms[0];
-form.onsubmit = function(evt) {
-  var url = 'http://nominatim.openstreetmap.org/search?format=json&q=';
-  url += form.query.value;var map = new ol.Map({
-  layers: [
-    new ol.layer.TileLayer({
-      source: new ol.source.OSM()
-    })
-  ],
-  renderers: ol.RendererHints.createFromQueryData(),
-  target: 'map',
-  view: new ol.View2D({
-    center: [0, 0],
-    zoom: 2
-  })
-});
-
 var geolocation = new ol.Geolocation();
 geolocation.bindTo('projection', map.getView());
 
@@ -83,17 +37,3 @@ geolocation.on('error', function(error) {
 $('#locate').click(function() {
   geolocation.setTracking(true);
 });
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", url, true);
-  xhr.onload = function() {
-    var result = JSON.parse(xhr.responseText);
-    if (result.length > 0) {
-      var bbox = result[0].boundingbox;
-      olMap.getView().fitExtent(ol.proj.transform([parseFloat(bbox[2]),
-          parseFloat(bbox[0]), parseFloat(bbox[3]), parseFloat(bbox[1])],
-          'EPSG:4326', 'EPSG:3857'), olMap.getSize());
-    }
-  };
-  xhr.send();
-  evt.preventDefault();
-};
