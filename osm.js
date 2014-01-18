3956ba2e7ae39e3a4b418e04b7755fa830bcacb6
tualex <scholz0alex@gmail.com>
2014-01-18 14:19:52 +0100

    Update osm.js

--- osm.js
+++ osm.js
@@ -21,8 +21,6 @@ geolocation.on('change:position', function setPosition() {
   olmap.getView().setCenter(geolocation.getPosition())
 });
 
-// an Geolocation gebundener Marker
-
 var marker = new ol.Overlay({
   positioning: ol.OverlayPositioning.CENTER_CENTER,
   element: document.getElementById('marker'),
@@ -31,102 +29,4 @@ var marker = new ol.Overlay({
 marker.bindTo('position', geolocation);
 olmap.addOverlay(marker);
 
-
-
-
-//Nominatim
-  
- var form = document.forms[0];
-form.onsubmit = function(auswerten) {
-  var url = 'http://nominatim.openstreetmap.org/search?';
-  var format = 'format=json';
-  var wert = 'q=' + form.angabe.value;
-  
-  if (form.aut.checked == true) {
-    var autwert = '&countrycodes=at';
-  } 
-  
-  else {var autwert = ''}
-   
-  var finalurl = url + wert + '&' + format + autwert;
-  
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", finalurl, true);
-  xhr.onload = function() {
-    var result = JSON.parse(xhr.responseText);
-    if (result.length > 0) {
-      var bbox = result[0].boundingbox;
-      var coordinates = ol.proj.transform([parseFloat(bbox[0])],
-          'EPSG:4326', 'EPSG:3857');
-      
-      var boundingbox = ol.proj.transform([parseFloat(bbox[2]),
-          parseFloat(bbox[0]), parseFloat(bbox[3]), parseFloat(bbox[1])],
-          'EPSG:4326', 'EPSG:3857');
-      
-      olmap.getView().fitExtent(boundingbox,olmap.getSize());
-      
-      
-    }
-  };
-  xhr.send();
-  auswerten.preventDefault();
-};
-
-
-// Add behaviour to the popup's close button
-var popupContainer = document.getElementById('popup');
-document.getElementById('popup-closer').onclick = function() {
-  popupContainer.style.display = 'none';
-  return false;
-};
-
-
- 
- 
-// Popup showing the position the user clicked
-
-var popup = new ol.Overlay({
-  element: document.getElementById('popup')
-});
-olmap.addOverlay(popup);
-
-olmap.on('singleclick', function(evt) {
-  popupContainer.style.display = 'block';
-  var element = popup.getElement();
-  var coordinate = evt.getCoordinate();
-  lonlat = ol.proj.transform(coordinate, 'EPSG:3857', 'EPSG:4326');
-  var hdms = ol.coordinate.toStringHDMS(ol.proj.transform(
-      coordinate, 'EPSG:3857', 'EPSG:4326'));
-
-  $(element).popover('destroy');
-  popup.setPosition(coordinate);
-  // the keys are quoted to prevent renaming in ADVANCED_OPTIMIZATIONS mode.
-  $(element).popover({
-    'placement': 'left',
-    'animation': false,
-    'html': true,
-    'content': '<p>The location you clicked was:</p><code>' + hdms + '</code>'
-  });
-  $(element).popover('show');
-});
-
-
-
-
-// Submit comment    
-
-document.commentform.onsubmit = function(eval) {
-  var url = 'b5_comment.php?comment=' + commentform.comment.value +
-      '&longitude=' + lonlat[0] + '&latitude=' + lonlat[1];
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onload = function() {
-    commentLayer.getSource().updateParams({});
-    alert(xhr.responseText);
-    commentform.comment.value = '';
-  };
-  xhr.send();
-  eval.preventDefault();
-
-};
 
