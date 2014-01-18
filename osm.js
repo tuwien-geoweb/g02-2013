625eeb31cbf9de8bb5ed96bdc07bcb4ba6803751
tualex <scholz0alex@gmail.com>
2014-01-18 14:12:34 +0100

    Update osm.js

--- osm.js
+++ osm.js
@@ -1,72 +1,163 @@
-var raster = new ol.layer.Tile({
-  source: new ol.source.TileJSON({
-    url: 'http://api.tiles.mapbox.com/v3/mapbox.geography-class.jsonp'
-  })
-});
+// Map-Elemente
 
-var style = new ol.style.Style({
-  symbolizers: [
-    new ol.style.Icon({
-      url: '../symbole/hund.png',
-      yOffset: -22
-    })
-  ]
-});
+    // Raster-Layer
+    
+    var rasterlayer = new ol.layer.Tile({
+      source: new ol.source.OSM()
+    });
+     
+               
+    // View
+    
+    var view = new ol.View2D({center: ol.proj.transform([16.37, 48.21], 'EPSG:4326', 'EPSG:3857'),
+    zoom: 12,
+    maxZoom: 18
+    });
+    
+   
 
-var vector = new ol.layer.Vector({
-  source: new ol.source.Vector({
-    features: [
-      new ol.Feature({
-        name: 'Null Island',
-        population: 4000,
-        rainfall: 500,
-        geometry: new ol.geom.Point([0, 0])
-      })
-    ]
-  }),
-  style: style
-});
 
-var map = new ol.Map({
-  layers: [raster, vector],
-  renderer: ol.RendererHint.CANVAS,
+// eigentliche Map
+
+var olmap = new ol.Map({
+  layers: [rasterlayer],
+  renderers: ol.RendererHints.CANVAS,
   target: 'map',
-  view: new ol.View2D({
-    center: [0, 0],
-    zoom: 3
-  })
+  view: view
 });
 
-var element = document.getElementById('popup');
 
-var popup = new ol.Overlay({
-  element: element,
-  positioning: ol.OverlayPositioning.BOTTOM_CENTER,
+
+ // Klickpunkte
+    
+    var commentLayer = new ol.layer.Image({
+  source: new ol.source.ImageWMS({
+    url: 'http://student.ifip.tuwien.ac.at/geoserver/g04_2013/wms?service=WMS&version=1.1.0&request=GetMap',
+    params: {'LAYERS': 'g04_2013:comments'}
+  })
+});
+olmap.addLayer(commentLayer);
+
+
+//Geolocation
+ 
+function geoloc(){    // damit �ber den Button aus aufrufbar
+
+var geolocation = new ol.Geolocation();     
+geolocation.bindTo('projection', view);
+geolocation.setTracking(true); 
+geolocation.on('change:position', function setPosition() {
+  olmap.getView().setCenter(geolocation.getPosition())
+});
+
+// an Geolocation gebundener Marker
+
+var marker = new ol.Overlay({
+  positioning: ol.OverlayPositioning.CENTER_CENTER,
+  element: document.getElementById('marker'),
   stopEvent: false
 });
-map.addOverlay(popup);
+marker.bindTo('position', geolocation);
+olmap.addOverlay(marker);
+}
+
+geoloc();
 
 
-map.on('singleclick', function(evt) {
-  map.getFeatures({
-    pixel: evt.getPixel(),
-    layers: [vector],
-    success: function(layerFeatures) {
-      var feature = layerFeatures[0][0];
-      if (feature) {
-        var geometry = feature.getGeometry();
-        var coord = geometry.getCoordinates();
-        popup.setPosition(coord);
-        $(element).popover({
-          'placement': 'top',
-          'html': true,
-          'content': feature.get('name')
-        });
-        $(element).popover('show');
-      } else {
-        $(element).popover('destroy');
-      }
+
+//Nominatim
+  
+ var form = document.forms[0];
+form.onsubmit = function(auswerten) {
+  var url = 'http://nominatim.openstreetmap.org/search?';
+  var format = 'format=json';
+  var wert = 'q=' + form.angabe.value;
+  
+  if (form.aut.checked == true) {
+    var autwert = '&countrycodes=at';
+  } 
+  
+  else {var autwert = ''}
+   
+  var finalurl = url + wert + '&' + format + autwert;
+  
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", finalurl, true);
+  xhr.onload = function() {
+    var result = JSON.parse(xhr.responseText);
+    if (result.length > 0) {
+      var bbox = result[0].boundingbox;
+      var coordinates = ol.proj.transform([parseFloat(bbox[0])],
+          'EPSG:4326', 'EPSG:3857');
+      
+      var boundingbox = ol.proj.transform([parseFloat(bbox[2]),
+          parseFloat(bbox[0]), parseFloat(bbox[3]), parseFloat(bbox[1])],
+          'EPSG:4326', 'EPSG:3857');
+      
+      olmap.getView().fitExtent(boundingbox,olmap.getSize());
+      
+      
     }
-  });
+  };
+  xhr.send();
+  auswerten.preventDefault();
+};
+
+
+// Add behaviour to the popup's close button
+var popupContainer = document.getElementById('popup');
+document.getElementById('popup-closer').onclick = function() {
+  popupContainer.style.display = 'none';
+  return false;
+};
+
+
+ 
+ 
+// Popup showing the position the user clicked
+
+var popup = new ol.Overlay({
+  element: document.getElementById('popup')
 });
+olmap.addOverlay(popup);
+
+olmap.on('singleclick', function(evt) {
+  popupContainer.style.display = 'block';
+  var element = popup.getElement();
+  var coordinate = evt.getCoordinate();
+  lonlat = ol.proj.transform(coordinate, 'EPSG:3857', 'EPSG:4326');
+  var hdms = ol.coordinate.toStringHDMS(ol.proj.transform(
+      coordinate, 'EPSG:3857', 'EPSG:4326'));
+
+  $(element).popover('destroy');
+  popup.setPosition(coordinate);
+  // the keys are quoted to prevent renaming in ADVANCED_OPTIMIZATIONS mode.
+  $(element).popover({
+    'placement': 'left',
+    'animation': false,
+    'html': true,
+    'content': '<p>The location you clicked was:</p><code>' + hdms + '</code>'
+  });
+  $(element).popover('show');
+});
+
+
+
+
+// Submit comment    
+
+document.commentform.onsubmit = function(eval) {
+  var url = 'b5_comment.php?comment=' + commentform.comment.value +
+      '&longitude=' + lonlat[0] + '&latitude=' + lonlat[1];
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.onload = function() {
+    commentLayer.getSource().updateParams({});
+    alert(xhr.responseText);
+    commentform.comment.value = '';
+  };
+  xhr.send();
+  eval.preventDefault();
+
+};
 
