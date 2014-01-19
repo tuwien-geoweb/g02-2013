810aef0e814a1db183242aa2aef225dbff414148
tualex <scholz0alex@gmail.com>
2014-01-19 14:18:54 +0100

    Update osm.js

--- osm.js
+++ osm.js
@@ -20,20 +20,6 @@ geolocation.on('change:position', function setPosition() {
   olmap.getView().setCenter(geolocation.getPosition())
 });
 
-var circle = new ol.Overlay({
-          map: map,
-          position: ol.proj.transform(
-              [parseFloat(lon), parseFloat(lat)], 'EPSG:4326', 'EPSG:3857'),
-          element: $('<div>').addClass('circle')
-      });
-function setCircleSize() {
-  var size = 2 * 400000 / map.getView().getResolution() + 'px';
-  $('.circle').width(size).height(size);
-}
-setCircleSize();
-map.getView().on('change:resolution', setCircleSize);
-olmap.addOverlay(circle);
-
 var marker = new ol.Overlay({
   map: map,
   position: ol.OverlayPositioning.CENTER_CENTER,
