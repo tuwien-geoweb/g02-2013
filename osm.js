c948bf91a020ba86a1bae638bcb7cc3ff21eadc9
tualex <scholz0alex@gmail.com>
2014-01-19 12:53:51 +0100

    Update osm.js

--- osm.js
+++ osm.js
@@ -76,7 +76,7 @@ form.onsubmit = function(evt) {
       var nominatim = new ol.Overlay({
           map: map,
           position: ol.proj.transform(
-              [16.3, 48.2], 'EPSG:4326', 'EPSG:3857'),
+              [lon, lat], 'EPSG:4326', 'EPSG:3857'),
           element: document.getElementById('nominatim')
       });
       olmap.addOverlay(nominatim);
